6ac699259b819c56b40c958c02d3cc3337469129
Slipzter <timhanssonmeng@gmail.com>
2023-09-05 11:55:05 +0200

    created base template spring backend

---
+++ server/src/main/java/com/example/pawpal/PawpalApplication.java
@@ -0,0 +1,13 @@
+package com.example.pawpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawpalApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/pawpal/PawpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pawpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
