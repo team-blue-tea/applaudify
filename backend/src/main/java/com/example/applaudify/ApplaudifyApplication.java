51f2430347840de4792fb41520525cd19aa0fd12
Ilija Krilovic <129670316+ica1130@users.noreply.github.com>
2023-09-11 13:45:04 +0200

    Folder backend
    Created a new project in Spring named Applaudify.

---
+++ backend/src/main/java/com/example/applaudify/ApplaudifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.applaudify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplaudifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplaudifyApplication.class, args);
+	}
+
+}
