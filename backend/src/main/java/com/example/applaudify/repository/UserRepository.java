1885e7c338dbd3f66dd02efe79d21a3a01c17be7
Slipzter <timhanssonmeng@gmail.com>
2023-09-13 12:23:02 +0200

    feat: connection to MongoDB Atlas

--- backend/src/main/java/com/example/applaudify/repository/UserRepository.java
+++ backend/src/main/java/com/example/applaudify/repository/UserRepository.java
@@ -2,8 +2,8 @@ package com.example.applaudify.repository;
 
 import com.example.applaudify.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
-
-
 }
---
+++ backend/src/main/java/com/example/applaudify/service/UserService.java
@@ -0,0 +1,27 @@
+package com.example.applaudify.service;
+
+import com.example.applaudify.User;
+import com.example.applaudify.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+}
