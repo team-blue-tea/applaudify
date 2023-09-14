ad0f652e0daa37032abbd14aec0fff1111a04385
ica1130 <ilijakrilovic@gmail.com>
2023-09-14 14:48:44 +0200

    added names in stead of ids to make it more simple for start.

--- backend/src/main/java/com/example/applaudify/Appreciation.java
+++ backend/src/main/java/com/example/applaudify/Appreciation.java
@@ -8,33 +8,33 @@ public class Appreciation {
 
     @Id
     private String id;
-    private String senderId;
-    private String receiverId;
+    private String senderName;
+    private String receiverName;
 //    private String userRelation;
 //    private String senderImageUrl;
 //    private String receiverImageUrl;
     private String comment;
 
-    public Appreciation(String senderId, String receiverId, String comment) {
-        this.senderId = senderId;
-        this.receiverId = receiverId;
+    public Appreciation(String senderName, String receiverName, String comment) {
+        this.senderName = senderName;
+        this.receiverName = receiverName;
         this.comment = comment;
     }
 
-    public String getSenderId() {
-        return senderId;
+    public String getSenderName() {
+        return senderName;
     }
 
-    public void setSenderId(String senderId) {
-        this.senderId = senderId;
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
     }
 
-    public String getReceiverId() {
-        return receiverId;
+    public String getReceiverName() {
+        return receiverName;
     }
 
-    public void setReceiverId(String receiverId) {
-        this.receiverId = receiverId;
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
     }
 
 //    public String getUserRelation() {
