9fb926b46fa9062e88b1d093c28729fec74f1aaa
Slipzter <timhanssonmeng@gmail.com>
2023-09-14 17:04:31 +0200

    added layout styling

--- client/app/(pages)/home/page.tsx
+++ client/app/(pages)/home/page.tsx
@@ -1,25 +1,22 @@
-'use client'
+"use client";
 
-import Navbar from "@/components/Navbar"
-import Link from "next/link"
 import { useEffect, useState } from "react";
-import { Appreciation } from "../types";
+import { Appreciation } from "../../types";
 import { AppreciationCard } from "@/components/AppreciationCard";
 
 function Home() {
-
   const [data, setData] = useState([]);
 
   const getAppreciation = async () => {
-    const response = await fetch('http://localhost:8081/appreciations');
+    const response = await fetch("http://localhost:8081/appreciations");
     const jsonData = await response.json();
     setData(jsonData);
     console.log(jsonData);
-  }
+  };
 
   useEffect(() => {
     getAppreciation();
-  }, [])
+  }, []);
 
   return (
     <>
@@ -28,13 +25,17 @@ function Home() {
         <ul>
           {data.map((element: Appreciation, index) => (
             <li key={index}>
-              <AppreciationCard senderName={element.senderName} receiverName={element.receiverName} comment={element.comment} />
+              <AppreciationCard
+                senderName={element.senderName}
+                receiverName={element.receiverName}
+                comment={element.comment}
+              />
             </li>
           ))}
         </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
---
+++ client/components/AppHeader.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Layout } from "antd";
+import "../app/globals.css";
+import Link from "next/link";
+
+const { Header } = Layout;
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <Header
+        style={{
+          position: "fixed",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          height: "4rem",
+        }}
+      >
+        <Link href={""} className="demo-logo">
+          Logo
+        </Link>
+        <div className="search-field">
+          <input type="text" />
+        </div>
+        <Link href={""} className="profile-icon">
+          Profile
+        </Link>
+
+        {/* <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        /> */}
+      </Header>
+    </Layout>
+  );
+};
+
+export default App;
