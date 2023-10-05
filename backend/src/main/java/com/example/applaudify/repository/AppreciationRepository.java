50a05f1ef333a974eba87d69ada727b64f64c8e1
ica1130 <ilijakrilovic@gmail.com>
2023-10-05 11:36:05 +0200

    feat: added endpoint to get appreciations by senderId and userId.

--- backend/src/main/java/com/example/applaudify/repository/AppreciationRepository.java
+++ backend/src/main/java/com/example/applaudify/repository/AppreciationRepository.java
@@ -3,6 +3,10 @@ package com.example.applaudify.repository;
 import com.example.applaudify.model.Appreciation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 
 public interface AppreciationRepository extends MongoRepository<Appreciation, String> {
+    List<Appreciation> findAllBySenderId(String senderId);
+    List<Appreciation> findAllByReceiverId(String receiverId);
 }
