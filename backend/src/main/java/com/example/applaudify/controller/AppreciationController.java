bd771dc634f9e0b37ea7eb7ecc70db6b252aaace
ica1130 <ilijakrilovic@gmail.com>
2023-10-16 11:17:07 +0200

    refactor: added version to Appretiations controller.

--- backend/src/main/java/com/example/applaudify/controller/AppreciationController.java
+++ backend/src/main/java/com/example/applaudify/controller/AppreciationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/appreciations")
+@RequestMapping("api/v1/appreciations")
 @CrossOrigin("*")
 public class AppreciationController {
 
